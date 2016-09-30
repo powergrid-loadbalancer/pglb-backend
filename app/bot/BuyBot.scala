20b8d51c7718ed23e4891bc48e9de35dafa2e3a4
LeanderK <Leander.Kurscheidt@gmx.de>
2016-09-30 06:29:23 +0200

    added totalBought

--- app/bot/BuyBot.scala
+++ app/bot/BuyBot.scala
@@ -2,11 +2,12 @@ package bot
 
 /**
   */
-case class BuyBot(maxAmount: Double, var depleted: Double) {
+case class BuyBot(maxAmount: Double, var depleted: Double, var totalBought: Double) {
   private var recentlyBought = false
 
   def buy(max: Double): Double = {
     val bought: Double = Math.min(max, (BotManager.capacity-depleted)*((Math.random() + Math.random())/2).toInt)
+    totalBought += bought + totalBought
     depleted = bought.toInt + depleted
     recentlyBought = true
     bought
